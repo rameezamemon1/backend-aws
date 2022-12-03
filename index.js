f838fa829edf936746eac937c90542df5a921b83
rameezamem1 <rameeza.memon1@gmail.com>
2022-12-03 23:28:11 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
